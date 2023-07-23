b5ff9bb9c76a2d47723ae86a274b284bc1dcb171
Fernando Ibarra <luisfernandoibarrao@gmail.com>
2023-07-23 18:36:39 -0400

    Cambio en Primer Test a Falso

--- tests/primer_Test.php
+++ tests/primer_Test.php
@@ -3,6 +3,6 @@ declare (strict_types=1);
 use PHPUnit\Framework\TestCase;
 class primer_Test extends TestCase{
     public function test_Primero():void{
-        $this->assertTrue(true);
+        $this->assertTrue(false);
     }
 }
